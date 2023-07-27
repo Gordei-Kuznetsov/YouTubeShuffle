d09bca0d61355728f633d3553fddb3d11b95d7fd
Gordei_K <gordei.ku.ro.ki@gmail.com>
2023-07-28 01:29:43 +1200

    add event handling

--- scripts/common/events.js
+++ scripts/common/events.js
@@ -4,64 +4,56 @@ export class Events{
     static onNext(func){
         currentPage = States.CurrentPage();
         if (currentPage == States.WatchPage){
-            /* add handler to:
-                - the next button click
-                - leaving the page after video finishes
-                -- general next event (like chrome in-built player thingy)
-            */
+            // on watch page next button click
+            document.getElementsByClassName("ytp-chrome-bottom ytp-next-button ytp-button")[0].onclick(func);
+            // on current video end
+            onRequest((url) => {
+                if (url.startsWith("https://www.youtube.com/youtubei/v1/next?key=")){
+                    func();
+                }
+            });
+            // add handler to general next event (like chrome in-built player thingy)
         }
         else if (this.isMiniplayerPresent()){
-            /* add handler to:
-                - the miniplayer next button click
-            */
+            // on miniplayer next button click
+            document.getElementsByClassName("ytp-miniplayer-ui ytp-next-button ytp-button")[0].onclick(func);
         }
     }
 
     static onPrevious(func){
         currentPage = States.CurrentPage();
         if (currentPage == States.WatchPage){
-            /* add handler to:
-                - the previous button click
-                -- general previous event (like chrome in-built player thingy)
-            */
+            // on watch page previous button click
+            document.getElementsByClassName("ytp-chrome-bottom ytp-prev-button ytp-button")[0].onclick(func);
+            // add handler to general previous event (like chrome in-built player thingy)
         }
         else if (this.isMiniplayerPresent()){
-            /* add handler to:
-                - the miniplayer previous button click
-            */
+            // on miniplayer previous button click
+            document.getElementsByClassName("ytp-miniplayer-ui ytp-prev-button ytp-button")[0].onclick(func);
         }
     }
 
     static onShuffle(func){
         currentPage = States.CurrentPage();
         if (currentPage == States.WatchPage){
-            /* 
-                Toggle shuffling ...
-            */
+            // on watch page shuffle button click
+            document.querySelectorAll('[aria-label="Shuffle playlist"]')[0].onclick(func);
         }
         else if (currentPage == States.PlaylistPage){
-            /*
-                Toggle shuffling ...
-            */
+            // on playlist page shuffle button click
+            document.getElementsByClassName("shuffle-button")[0].onclick(func);
         }
     }
 
     static isMiniplayerPresent(){
-        // check if miniplayer exists 
+        return document.getElementsByTagName("ytd-miniplayer[active]")[0] != null;
+    }
+
+    static onRequest(func){
+        var originalOpen = XMLHttpRequest.prototype.open;
+        XMLHttpRequest.prototype.open = function(method, url) {
+            func(url);
+            originalOpen.apply(this, arguments);
+        };
     }
 }
-
-
-/*
-Playlist page
-let shuffleButton = document.getElementsByClassName("shuffle-button")
-let nextButton = document.getElementsByClassName("ytp-next-button ytp-button")
-let prevButton = document.getElementsByClassName("ytp-prev-button ytp-button")
-let miniplayer = document.getElementsByTagName("ytd-miniplayer")
-
-
-Watch page
-let shuffleButton = document.querySelectorAll('[aria-label="Shuffle playlist"]')
-let nextButton = document.getElementsByClassName("ytp-next-button ytp-button")
-let prevButton = document.getElementsByClassName("ytp-prev-button ytp-button")
-*/
